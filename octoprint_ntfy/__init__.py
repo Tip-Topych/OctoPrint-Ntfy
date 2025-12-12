2c811fd80242aea1727c652e88e7d2a56bd5ee2b
Tip-Topych <padla666@list.ru>
2025-12-12 19:10:38 +0700

    update plugin requires

--- octoprint_ntfy/__init__.py
+++ octoprint_ntfy/__init__.py
@@ -136,12 +136,11 @@ class NtfyPlugin(octoprint.plugin.StartupPlugin,
 __plugin_name__ = "Ntfy Notification"
 __plugin_pythoncompat__ = ">=3,<4"
 
-
 def __plugin_load__():
     global __plugin_implementation__
     __plugin_implementation__ = NtfyPlugin()
 
     global __plugin_hooks__
     __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+        # Можно добавить хуки обновлений, если нужно, но пока оставим пустым или базовым
     }
--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
 # setup.py
+from setuptools import setup
+
 plugin_identifier = "ntfy"
 plugin_package = "octoprint_ntfy"
 plugin_name = "OctoPrint-Ntfy"
 plugin_version = "0.1.0"
-plugin_description = "Отправляет уведомления и скриншоты в self-hosted ntfy"
-plugin_author = "Ваше Имя"
+plugin_description = "Integration with self-hosted ntfy"
+plugin_author = "Your Name"
 plugin_author_email = "your@email.com"
 plugin_url = "https://github.com/yourusername/OctoPrint-Ntfy"
 plugin_license = "AGPLv3"
 
-from setuptools import setup
+plugin_requires = ["requests", "eventlet"]
 
-def plugin_data(pkg, *dirs):
-    import os
-    data = []
-    for d in dirs:
-        for walk in os.walk(os.path.join(pkg, d)):
-            data.append(walk[0][len(pkg) + 1 :] + "/*")
-    return data
+plugin_additional_data = [] # MANIFEST.in handles templates now
 
 setup_parameters = dict(
     name=plugin_name,
@@ -28,10 +24,9 @@ setup_parameters = dict(
     url=plugin_url,
     license=plugin_license,
     packages=[plugin_package],
-    package_data={plugin_package: plugin_data(plugin_package, "templates")},
-    include_package_data=True,
+    include_package_data=True, # Важно для MANIFEST.in
     zip_safe=False,
-    install_requires=["requests"],  # Нам нужна эта библиотека
+    install_requires=plugin_requires,
     entry_points={
         "octoprint.plugin": [
             "ntfy = octoprint_ntfy"
